c9362bd4a53e343e0392aa67af67086da0c47fbe
Omar <omar0642624692@gmail.com>
2023-10-29 16:05:53 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,15 +8,18 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close(exception):
     """close session when finished"""
     storage.close()
 
+
 @app.errorhandler(404)
 def invalid_route(e):
     """handle 404 error"""
     return ({"error": "Not found"}, 404)
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
