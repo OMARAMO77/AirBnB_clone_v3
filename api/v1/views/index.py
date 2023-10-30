0472a8c855637623d960af6dedc0972c725df108
Omar <omar0642624692@gmail.com>
2023-10-30 09:18:28 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,11 +21,11 @@ def status():
 def get_stats():
     """Retrieve the number of each objects by type"""
     classes = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
     }
     return jsonify(classes)
