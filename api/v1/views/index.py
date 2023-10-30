ad46e3395c3db56d3a9786e8fdb39655ad6c2794
Omar <omar0642624692@gmail.com>
2023-10-30 09:14:39 -0700

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """---"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, Blueprint
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -14,8 +20,12 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """Retrieve the number of each objects by type"""
-    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
-               "reviews": 'Review', "states": 'State', "users": 'User'}
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
+    classes = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(classes)
